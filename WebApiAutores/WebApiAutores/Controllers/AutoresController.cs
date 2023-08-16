5aa10c1d7b476f9a36e7db674e4a4728be910815
saulnunezgarcia <saulgarcia.2398@gmail.com>
2023-08-16 17:59:06 -0600

    Course for we Apis

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController] // Hace validaciones automaticas 
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+
+    {
+        [HttpGet] 
+        public ActionResult<List<Autor>> Get() 
+        {
+            return new List<Autor>() {
+                new Autor() { Id = 1, Nombre = "Felipe" },
+                new Autor() { Id = 2, Nombre = "Claudia" },
+            };
+        }
+            
+    }
+}
